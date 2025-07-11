34e6d5e3f2f1b5fd43497a618a825499c19b35f5
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-07-11 21:58:44 +0300

    stack lab done

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,50 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = []
+        self.limit = limit
+
+        if items:
+                if len(items) > limit:
+                    raise ValueError("Too many items for the stack limit.")
+                self.items = items.copy()
+
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if self.full():
+            return None
+        self.items.append(item)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        return None
 
     def peek(self):
-        pass
+        if self.isEmpty():
+              raise IndexError("Cannot peek from an empty stack")
+        return self.items[-1]
     
     def size(self):
-        pass
-
+            return len(self.items)
     def full(self):
-        pass
-
+        return self.size() == self.limit
+    
     def search(self, target):
-        pass
+        for i in range(len(self.items)-1, -1,-1):
+             if self.items[i] == target:
+                  return len(self.items)-1-i
+        return -1
+    
+
+
+s = Stack([1,2,3,4,5], limit =5)
+print(s.items)
+print(s.limit)
+print(s.size())
+print(s.pop())
+print(s.full())
+print(s.search(4))
